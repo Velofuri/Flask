b667084bfaa10386f68afa2600aec6bac57fd334
Rafael <velofuri@gmail.com>
2022-11-30 21:19:44 -0300

    codigo JS adicionado

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
+from helpers import recupera_imagem
+import time
 
 @app.route("/")
 def index():
@@ -31,7 +33,8 @@ def criar():
 
     arquivo = request.files['arquivo']
     upload_path = app.config['UPLOAD_PATH']
-    arquivo.save(f'{upload_path}/capa{novo_jogo.id}.jpg')
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{novo_jogo.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
 
@@ -40,7 +43,8 @@ def editar(id):
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = Jogos.query.filter_by(id=id).first()
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+    capa_jogo = recupera_imagem(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
 
 
 @app.route('/atualizar', methods=['POST'])
@@ -52,8 +56,13 @@ def atualizar():
 
     db.session.add(jogo)
     db.session.commit()
-    flash(f'Jogo {jogo.nome} atualizado com sucesso!')
 
+    arquivo = request.files['arquivo']
+    upload_path = app.config['UPLOAD_PATH']
+    timestamp = time.time()
+    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+
+    
     return redirect(url_for('index'))
 
 @app.route('/deletar/<int:id>')
@@ -99,3 +108,4 @@ def logout():
 @app.route('/uploads/<nome_arquivo>')
 def imagem(nome_arquivo):
     return send_from_directory('uploads', nome_arquivo)
+
